59912d1ead33aca2f4516b98caadb556a129a04f
Anthony Rolland <6172844+LaconicPneumonic@users.noreply.github.com>
2023-09-26 21:43:34 -0400

    use langchain to split contexts (#3)
    * use langchain
    
    * pass wikipedia article by title

--- corpus_query/services/api/main.py
+++ corpus_query/services/api/main.py
@@ -2,11 +2,15 @@ import logging
 import random
 from typing import List, Optional, Tuple
 
-import wikipedia
 from fastapi import FastAPI, File, UploadFile
+from langchain.document_loaders import PyPDFLoader
 from pydantic import BaseModel
 
-from corpus_query.services.ingestion.chunk import chunk_file_by_line
+from corpus_query.services.ingestion.chunk import (
+    ChunkingOptions,
+    FileTypes,
+    chunk_by_file_type,
+)
 from corpus_query.services.vector_db.client import ArticleSnippet, VectorDbClient
 
 app = FastAPI()
@@ -18,6 +22,7 @@ class CorpusRequest(BaseModel):
 
 class QuestionRequest(BaseModel):
     question: str
+    limit: Optional[int] = 5
 
 
 class WikiRequest(BaseModel):
@@ -27,62 +32,49 @@ class WikiRequest(BaseModel):
 logging.basicConfig(level=logging.INFO)
 
 
-def get_chunked_contents(article_title) -> Optional[Tuple[List[str], str]]:
-    try:
-        try:
-            article_content = wikipedia.page(article_title)
-        except wikipedia.DisambiguationError as e:
-            s = random.choice(e.options)
-            article_content = wikipedia.page(s)
-
-        chunked_content = list(chunk_file_by_line(article_content.content))
-
-        return chunked_content, article_content.url
-
-    except Exception as e:
-        logging.error("error getting chunked content for %s", article_title)
-
-
 @app.get("/")
 def get_hello_world():
     return "Hello World"
 
 
 @app.post("/load_wikipedia")
-def load_wikipedia_corpus(corpus: WikiRequest):
+def load_wikipedia_corpus(corpus: WikiRequest, options: Optional[ChunkingOptions]):
     c = VectorDbClient()
-
     c.create_schema()
 
-    article_titles = (wikipedia.search(article)[0] for article in corpus.article_names)
-
-    chunked_contents = (
-        (article.title(), get_chunked_contents(article.title()))
-        for article in article_titles
-    )
-
     ret = []
-    for article_title, content_pair in chunked_contents:
-        if content_pair is None:
-            ret.append(f"failed to get content for '{article_title}'")
-            continue
 
-        chunked_content, url = content_pair
-        article_snippets = [
-            ArticleSnippet(article_title, snippet) for snippet in chunked_content
-        ]
+    for article_title in corpus.article_names:
+        query = f"https://en.wikipedia.org/api/rest_v1/page/pdf/{article_title}"
+        logging.info("loading '%s'", query)
+        loader = PyPDFLoader(query)
 
-        c.upload_data(article_snippets)
+        documents = loader.load_and_split()
+
+        logging.info("loaded '%s'", query)
+
+        snippets = []
+        for d in documents:
+            for snippet in chunk_by_file_type(d.page_content, FileTypes.PDF, options):
+                logging.info(snippet)
+                snippets.append(snippet)
+
+        logging.info("snipped '%s'", query)
+
+        c.upload_data([ArticleSnippet(article_title, d.page_content) for d in snippets])
+        logging.info("done w/ '%s'", query)
 
         ret.append(
-            f"uploaded '{article_title}' ({url}) with {len(article_snippets)} snippets"
+            f"uploaded '{article_title}' ({query}) with {len(snippets)} snippets"
         )
 
     return {"message": ret}
 
 
 @app.post("/corpus")
-def add_corpus(file: UploadFile):
+def add_corpus(
+    file: UploadFile, file_type: FileTypes, options: Optional[ChunkingOptions] = None
+):
     logging.info("received file: '%s'", file.filename)
 
     c = VectorDbClient()
@@ -90,7 +82,7 @@ def add_corpus(file: UploadFile):
 
     article_snippets = list(
         ArticleSnippet(file.filename, snippet)
-        for snippet in chunk_file_by_line(file.file.read().decode())
+        for snippet in chunk_by_file_type(file.file.read().decode(), file_type, options)
     )
 
     logging.info("chunked file into %d lines", len(article_snippets))
@@ -105,4 +97,4 @@ def add_corpus(file: UploadFile):
 def ask_question(question: QuestionRequest):
     c = VectorDbClient()
 
-    return c.query_from_string(question.question, limit=5)
+    return c.query_from_string(question.question, limit=question.limit)
