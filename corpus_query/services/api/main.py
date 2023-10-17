3bcfac8de75c010814af680e7512d3c211605c3a
Anthony Rolland <6172844+LaconicPneumonic@users.noreply.github.com>
2023-10-16 20:07:01 -0400

    Use Lllama Index (#4)
    * Use Lllama Index
    
    * top qual

--- corpus_query/services/api/main.py
+++ corpus_query/services/api/main.py
@@ -1,35 +1,71 @@
 import logging
-import random
-from typing import List, Optional, Tuple
+import os
+from typing import List
 
-from fastapi import FastAPI, File, UploadFile
+import weaviate
+from fastapi import FastAPI, UploadFile
 from langchain.document_loaders import PyPDFLoader
+from llama_index import (
+    Document,
+    ServiceContext,
+    StorageContext,
+    VectorStoreIndex,
+    set_global_service_context,
+)
+from llama_index.llms import OpenAI
+from llama_index.node_parser import SimpleNodeParser
+from llama_index.schema import BaseNode
+from llama_index.vector_stores import WeaviateVectorStore
 from pydantic import BaseModel
 
-from corpus_query.services.ingestion.chunk import (
-    ChunkingOptions,
-    FileTypes,
-    chunk_by_file_type,
+logging.basicConfig(level=logging.INFO)
+
+# connect to your weaviate instance
+# define LLM
+llm = OpenAI(
+    model="gpt-4",
+    temperature=0,
+    max_tokens=256,
+    api_key=os.getenv("OPENAI_API_KEY"),
 )
-from corpus_query.services.vector_db.client import ArticleSnippet, VectorDbClient
+
+# configure service context
+service_context = ServiceContext.from_defaults(llm=llm)
+set_global_service_context(service_context)
+
+client = weaviate.Client(
+    url=os.getenv("VECTOR_DB_URL"),
+    # embedded_options=weaviate.embedded.EmbeddedOptions(),
+    additional_headers={"X-OpenAI-Api-Key": os.getenv("OPENAI_API_KEY")},
+)
+
+
+# construct vector store
+vector_store = WeaviateVectorStore(
+    weaviate_client=client, index_name="BlogPost", text_key="content"
+)
+
+# setting up the storage for the embeddings
+storage_context = StorageContext.from_defaults(vector_store=vector_store)
+
+# set up the index
+index = VectorStoreIndex.from_vector_store(vector_store)
+
 
 app = FastAPI()
 
 
-class CorpusRequest(BaseModel):
-    corpus: str
+def chunk_by_file_type(docs: List[Document]) -> List[BaseNode]:
+    parser = SimpleNodeParser.from_defaults()
+    return parser.get_nodes_from_documents(docs)
 
 
 class QuestionRequest(BaseModel):
     question: str
-    limit: Optional[int] = 5
 
 
 class WikiRequest(BaseModel):
     article_names: List[str]
-
-
-logging.basicConfig(level=logging.INFO)
 
 
 @app.get("/")
@@ -38,7 +74,7 @@ def get_hello_world():
 
 
 @app.post("/load_wikipedia")
-def load_wikipedia_corpus(corpus: WikiRequest, options: Optional[ChunkingOptions]):
+def load_wikipedia_corpus(corpus: WikiRequest):
     """Input a list of wikipedia article names and load them into the vector db.
 
     Args:
@@ -48,62 +84,55 @@ def load_wikipedia_corpus(corpus: WikiRequest, options: Optional[ChunkingOptions
     Returns:
         Dict: Response document
     """
-    c = VectorDbClient()
-    c.create_schema()
 
+    global index
     ret = []
 
     for article_title in corpus.article_names:
-        query = f"https://en.wikipedia.org/api/rest_v1/page/pdf/{article_title}"
-        logging.info("loading '%s'", query)
-        loader = PyPDFLoader(query)
+        try:
+            query = f"https://en.wikipedia.org/api/rest_v1/page/pdf/{article_title}"
+            logging.info("loading '%s'", query)
+            loader = PyPDFLoader(query)
 
-        documents = loader.load_and_split()
+            documents = loader.load_and_split()
+            logging.info("loaded '%s'", query)
+            snippets = chunk_by_file_type(
+                [Document(text=doc.page_content) for doc in documents]
+            )
+            logging.info("snipped '%s'", query)
 
-        logging.info("loaded '%s'", query)
+            logging.info("done w/ '%s'", query)
 
-        snippets = []
-        for d in documents:
-            for snippet in chunk_by_file_type(d.page_content, FileTypes.PDF, options):
-                logging.info(snippet)
-                snippets.append(snippet)
+            ret.append(
+                f"uploaded '{article_title}' ({query}) with {len(snippets)} snippets"
+            )
 
-        logging.info("snipped '%s'", query)
-
-        c.upload_data([ArticleSnippet(article_title, d.page_content) for d in snippets])
-        logging.info("done w/ '%s'", query)
-
-        ret.append(
-            f"uploaded '{article_title}' ({query}) with {len(snippets)} snippets"
-        )
+        except Exception as e:
+            logging.exception(e)
+            ret.append(f"failed to upload '{article_title}' using ({query})")
 
     return {"message": ret}
 
 
 @app.post("/corpus")
-def add_corpus(
-    file: UploadFile, file_type: FileTypes, options: Optional[ChunkingOptions] = None
-):
+def add_corpus(file: UploadFile):
+    global index
     logging.info("received file: '%s'", file.filename)
 
-    c = VectorDbClient()
-    c.create_schema()
+    d = Document(text=file.file.read().decode())
 
-    article_snippets = list(
-        ArticleSnippet(file.filename, snippet)
-        for snippet in chunk_by_file_type(file.file.read().decode(), file_type, options)
-    )
+    snippets = chunk_by_file_type([d])
 
-    logging.info("chunked file into %d lines", len(article_snippets))
+    logging.info("chunked file into %d lines", len(snippets))
 
-    c.upload_data(article_snippets)
-    return {
-        "message": f"uploaded {file.filename} with {len(article_snippets)} snippets"
-    }
+    index.insert_nodes(snippets)
+
+    return {"message": f"uploaded {file.filename} with {len(snippets)} snippets"}
 
 
 @app.post("/corpus/question")
 def ask_question(question: QuestionRequest):
-    c = VectorDbClient()
+    global index
+    query_engine = index.as_query_engine()
 
-    return c.query_from_string(question.question, limit=question.limit)
+    return {"response": str(query_engine.query(question.question))}
